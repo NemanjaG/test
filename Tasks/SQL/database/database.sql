b7ebfde916bc9e160a2f214ff4f5137cbce7de0c
Goran <gorandjordjevic@canicinteractive.com>
2018-01-26 12:08:53 +0100

    Docker

---
+++ Tasks/SQL/database/database.sql
@@ -0,0 +1,25 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: mariadb
+-- Generation Time: Jan 26, 2018 at 11:02 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.0.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `TEST`
+--
+CREATE DATABASE IF NOT EXISTS `TEST` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `TEST`;
